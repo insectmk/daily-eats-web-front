ea0fde17633f7362f614aad78a67545ade735478
makuning <makun_ing@126.com>
2024-10-18 17:45:50 +0800

    为axios请求增加请求、响应拦截

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// 创建axios对象
 const axiosInstance = axios.create({
   baseURL: 'https://localhost:8001', // 你的 API 基础 URL
   timeout: 10000, // 请求超时时间
@@ -7,5 +8,35 @@ const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+// 请求拦截，文档：https://www.axios-http.cn/docs/interceptors
+axiosInstance.interceptors.request.use(config => {
+  // 检查登录状态
+  const jwtToken = localStorage.getItem('jwtToken')
+  if (jwtToken) {
+    config.headers['Authorization'] = 'Bearer ' + token
+  }
+  return config
+}, error => {
+  console.log(error)
+  return Promise.reject(error)
+})
+
+// 响应拦截
+axiosInstance.interceptors.response.use(response => {
+  // 2xx 范围内的状态码都会触发该函数。
+  const code = response.data.code
+  // 登录校验
+  switch (code) {
+    case 401: // 未登录
+      location.href = '/login'
+      break
+  }
+  return response
+}, function (error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  return Promise.reject(error)
+})
 
 export default axiosInstance;
