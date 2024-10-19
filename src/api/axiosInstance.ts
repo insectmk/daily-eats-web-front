c227a649f799f98de55654e3d4f9e7b1c4f7ff37
makuning <makun_ing@126.com>
2024-10-19 10:05:29 +0800

    开发接口改为http

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 创建axios对象
 const axiosInstance = axios.create({
-  baseURL: 'https://localhost:8001', // 你的 API 基础 URL
+  baseURL: 'http://localhost:8001', // 你的 API 基础 URL
   timeout: 10000, // 请求超时时间
   headers: {
     'Content-Type': 'application/json',
