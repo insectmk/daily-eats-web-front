5f3f738f7740aa9e0b0848d4773a97de6646f5ff
makuning <makun_ing@126.com>
2024-10-19 11:11:41 +0800

    调整代码格式

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import store from './store'
-
 import '@/styles/index.scss' // 全局CSS
 
 const app = createApp(App)
 
+// 注册插件
 app.use(router)
 app.use(ElementPlus)
 app.use(store)
-
+// 挂载
 app.mount('#app')
