d90b83d9c91e1ac178ce5405619db1144c361abb
makuning <makun_ing@126.com>
2024-10-29 14:49:24 +0800

    全局配置ElementUI图标组件

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,19 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import store from './store'
 import '@/styles/index.scss' // 全局CSS
+import * as ElementPlusIconsVue from '@element-plus/icons-vue' // Element-Plus图标
 
 const app = createApp(App)
+
+// 全局组件
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {// 全局注册Element-Plus图标
+  app.component(key, component)
+}
 
 // 注册插件
 app.use(router)
 app.use(ElementPlus)
 app.use(store)
+
 // 挂载
 app.mount('#app')
